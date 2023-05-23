c918cad9bc0e0e27fa5b61f2e375b41f41929dd6
Ayayehia <120187793+Ayayehia@users.noreply.github.com>
2023-05-23 20:24:09 +0300

    Add files via upload

---
+++ Algorithms/algo.js
@@ -0,0 +1,25 @@
+var str1 = "I Love gomycode .";
+var count_spaces = 0;
+var count_vowels = 0;
+
+const Test = (str) => {
+  while (str[i] !== ".") {
+    if (str[i] == " ") count_spaces++;
+    if (
+      str[i].lowercase() == "a" ||
+      str[i].lowercase() == "e" ||
+      str[i].lowercase() == "o" ||
+      str[i].lowercase() == "u" ||
+      str[i].lowercase() == "i"
+    )
+      count_vowelsl++;
+  }
+};
+test(str1);
+
+var arr1 = [3, 1, 7, 9];
+var arr2 = [2, 4, 1, 9, 3];
+var_left = arr1.filter((arr) => {
+  return arr1.indexOf(arr) < 0;
+});
+console.log(arr_left.concat(arr_right));
